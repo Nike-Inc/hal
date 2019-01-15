8f801140f96865ea1ffe67fb1492fa484a06742d
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2019-01-14 19:19:46 -0800

    Add custom authorizer and correctly identify nullable fields.

--- src/AWS/Lambda/Events/ApiGatewayProxyRequest.hs
+++ src/AWS/Lambda/Events/ApiGatewayProxyRequest.hs
@@ -18,29 +18,30 @@ import           Data.Text    (Text)
 import           GHC.Generics (Generic (..))
 
 data Identity = Identity
-    { cognitoIdentityPoolId         :: Text
-    , accountId                     :: Text -- Same as before
-    , cognitoIdentityId             :: Text
-    , caller                        :: Text -- what is this?
-    , apiKey                        :: Text
-    , sourceIp                      :: Text -- typeable?
-    , cognitoAuthenticationType     :: Text -- typeable? are this limited and well defined?
-    , cognitoAuthenticationProvider :: Text -- typeable? are this limited and well defined?
-    , userArn                       :: Text -- does amazonka type these?
-    , userAgent                     :: Text -- can't imagine there's an effective way to type these
-    , user                          :: Text -- no idea what this is
+    { cognitoIdentityPoolId         :: Maybe Text
+    , accountId                     :: Maybe Text
+    , cognitoIdentityId             :: Maybe Text
+    , caller                        :: Maybe Text
+    , apiKey                        :: Maybe Text
+    , sourceIp                      :: Text
+    , cognitoAuthenticationType     :: Maybe Text
+    , cognitoAuthenticationProvider :: Maybe Text
+    , userArn                       :: Maybe Text
+    , userAgent                     :: Maybe Text
+    , user                          :: Maybe Text
     } deriving (Generic)
 
 instance FromJSON Identity
 
 data RequestContext = RequestContext
-    { accountId    :: Text -- This is well understood, does amazonka have a type for this?
-    , resourceId   :: Text -- what is this?
+    { accountId    :: Text
+    , authorizer   :: Map Text Text
+    , resourceId   :: Text
     , stage        :: Text
-    , requestId    :: Text -- is there a UUID type?
+    , requestId    :: Text
     , identity     :: Identity
-    , resourcePath :: Text -- url path?
-    , httpMethod   :: Text --method?
+    , resourcePath :: Text
+    , httpMethod   :: Text
     , apiId        :: Text
     } deriving (Generic)
 
@@ -48,14 +49,14 @@ instance FromJSON RequestContext
 
 data ApiGatewayProxyRequest a = ApiGatewayProxyRequest
     { path                            :: Text
-    , headers                         :: [(Text, Text)] -- Might be nullable, but it seems like it _couldn't_ ever be empty
-    , multiValueHeaders               :: [(Text, [Text])] -- See "headers"
-    , pathParameters                  :: [(Text, Text)] -- Can be null
+    , headers                         :: Maybe [(Text, Text)]
+    , multiValueHeaders               :: Maybe [(Text, [Text])]
+    , pathParameters                  :: Maybe [(Text, Text)]
     , requestContext                  :: RequestContext
     , resource                        :: Text
     , httpMethod                      :: Text
-    , queryStringParameters           :: [(Text, Text)] -- Can be null
-    , multiValueQueryStringParameters :: [(Text, Text)] -- Can be null
+    , queryStringParameters           :: Maybe [(Text, Text)]
+    , multiValueQueryStringParameters :: Maybe [(Text, [Text])]
     , isBase64Encoded                 :: Bool
     , body                            :: a
     } deriving (Generic)
