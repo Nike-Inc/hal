dcb69c31437f5d169713e5f5182f6cac4e281421
Fernando Freire <fernando.freire@nike.com>
2019-02-25 18:47:33 -0800

    Add better S3 event implementation

--- src/AWS/Lambda/Events/S3.hs
+++ src/AWS/Lambda/Events/S3.hs
@@ -1,5 +1,5 @@
 {-|
-Module      : AWS.Lambda.Events.S3.BaseEvent
+Module      : AWS.Lambda.Events.S3
 Description : Data types for working with S3 events.
 Copyright   : (c) Nike, Inc., 2019
 License     : BSD3
@@ -7,26 +7,30 @@ Maintainer  : nathan.fairhurst@nike.com, fernando.freire@nike.com
 Stability   : stable
 -}
 
-module AWS.Lambda.Events.S3.BaseEvent (
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module AWS.Lambda.Events.S3 (
   PrincipalIdentity(..),
-  BaseEvent(..),
   Records(..),
   RequestParameters(..),
   ResponseElements(..),
   S3Bucket(..),
-  S3Config(..)
+  S3Config(..),
+  S3Event(..),
+  S3Object(..)
 ) where
 
-import Data.Aeson   (FromJSON (..), withObject, (.:))
-import Data.Text    (Text)
-import Data.Time.Clock (UTCTime)
-import GHC.Generics (Generic)
+import Data.Aeson       (FromJSON (..), Value (Object), withObject, (.:), (.:?))
+import Data.Aeson.Types (typeMismatch)
+import Data.Text        (Text)
+import Data.Time.Clock  (UTCTime)
+import GHC.Generics     (Generic)
 
-newtype Records a = Records {
-  records :: [BaseEvent a]
+newtype Records = Records {
+  records :: [S3Event]
 } deriving (Show, Eq)
 
-instance (FromJSON a) => FromJSON (Records a) where
+instance FromJSON Records where
   parseJSON = withObject "Records" $ \v -> Records <$> v .: "Records"
 
 data PrincipalIdentity = PrincipalIdentity {
@@ -43,14 +47,14 @@ data S3Bucket = S3Bucket {
 
 instance FromJSON S3Bucket
 
-data S3Config a = S3Config {
+data S3Config = S3Config {
   bucket          :: S3Bucket,
   configurationId :: Text,
-  object          :: a,
+  object          :: S3Object,
   s3SchemaVersion :: Text
 } deriving (Show, Eq, Generic)
 
-instance (FromJSON a) => FromJSON (S3Config a)
+instance FromJSON S3Config
 
 data ResponseElements = ResponseElements {
   amazonId        :: Text,
@@ -67,7 +71,8 @@ data RequestParameters = RequestParameters {
 
 instance FromJSON RequestParameters
 
-data BaseEvent a = BaseEvent {
+-- | Event data sent by S3 when triggering a Lambda.
+data S3Event = S3Event {
   awsRegion         :: Text,
   eventName         :: Text,
   eventSource       :: Text,
@@ -75,8 +80,35 @@ data BaseEvent a = BaseEvent {
   eventVersion      :: Text,
   requestParameters :: RequestParameters,
   responseElements  :: ResponseElements,
-  s3                :: S3Config a,
+  s3                :: S3Config,
   userIdentity      :: PrincipalIdentity
 } deriving (Show, Eq, Generic)
 
-instance (FromJSON a) => FromJSON (BaseEvent a)
+instance FromJSON S3Event
+
+-- | S3 object representations based on event type received.
+--
+-- Currently only Put/Delete events can trigger Lambdas
+data S3Object =
+  PutObject {
+    eTag      :: Text,
+    sequencer :: Text,
+    key       :: Text,
+    size      :: Int
+  } | DeleteObject {
+    sequencer :: Text,
+    key       :: Text
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON S3Object where
+  parseJSON (Object o) = do
+    maybeEtag  <- o .:? "eTag"
+    maybeSize  <- o .:? "size"
+    key'       <- o .:  "key"
+    sequencer' <- o .:  "sequencer"
+
+    return $ case (maybeEtag, maybeSize) of
+      (Just etag', Just size') -> PutObject etag' sequencer' key' size'
+      _                        -> DeleteObject sequencer' key'
+
+  parseJSON invalid    = typeMismatch "S3Object" invalid
