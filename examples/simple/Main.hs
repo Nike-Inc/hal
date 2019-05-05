2497bb0d58d5160a4cef8467c772b4519b1ac763
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2019-05-05 13:00:37 -0700

    Use a LambdaSerializable class to help safely encode a variety of content types into the body field of the JSON returned in an ApiGatewayProxyResponse.

--- examples/simple/Main.hs
+++ examples/simple/Main.hs
@@ -3,14 +3,15 @@
 
 module Main where
 
-import           AWS.Lambda.ApiGatewayRuntime              (pureRuntime)
 import           AWS.Lambda.Events.ApiGatewayProxyRequest  (ApiGatewayProxyRequest (ApiGatewayProxyRequest))
 import           AWS.Lambda.Events.ApiGatewayProxyResponse (ApiGatewayProxyResponse (ApiGatewayProxyResponse))
+import           AWS.Lambda.Runtime                        (pureRuntime)
 import           Data.Aeson                                (FromJSON, ToJSON)
+import           Data.Text                                 (Text)
 import           GHC.Generics                              (Generic)
 import           Network.HTTP.Types.Status                 (ok200)
 
-handler :: ApiGatewayProxyRequest String -> ApiGatewayProxyResponse
+handler :: ApiGatewayProxyRequest -> ApiGatewayProxyResponse Text
 handler x =
     ApiGatewayProxyResponse 200 mempty "Hello from Haskell Lambda"
 
--- src/AWS/Lambda/Events/ApiGatewayProxyResponse.hs
+++ src/AWS/Lambda/Events/ApiGatewayProxyResponse.hs
@@ -10,18 +10,74 @@ Stability   : stable
 -}
 module AWS.Lambda.Events.ApiGatewayProxyResponse
     ( ApiGatewayProxyResponse(..)
+    , LambdaSerializable(..)
+    , applicationJson
+    , imageGif
+    , imageJpeg
     ) where
 
-import           Data.Aeson          (ToJSON)
-import           Data.HashMap.Strict (HashMap)
-import qualified Data.Text.Lazy      as TL
-import qualified Data.Text           as T
-import           GHC.Generics        (Generic (..))
+import           Data.Aeson              (ToJSON, encode, object, toJSON, (.=))
+import           Data.ByteString         (ByteString)
+import qualified Data.ByteString.Base64  as B64
+import           Data.HashMap.Strict     (HashMap, insertWith)
+import qualified Data.Text               as T
+import qualified Data.Text.Encoding      as TE
+import qualified Data.Text.Lazy          as TL
+import qualified Data.Text.Lazy.Encoding as TLE
 
-data ApiGatewayProxyResponse = ApiGatewayProxyResponse
+data ApiGatewayProxyResponse a = ApiGatewayProxyResponse
     { statusCode :: Int
     , headers    :: HashMap T.Text T.Text
-    , body       :: TL.Text
-    } deriving (Show, Generic)
+    , body       :: a
+    } deriving Show
 
-instance ToJSON ApiGatewayProxyResponse
+class LambdaSerializable a where
+  serialize :: a -> T.Text
+  contentType :: a -> T.Text
+
+instance LambdaSerializable T.Text where
+  serialize = id
+  contentType _ = "text/plain; charset=utf8"
+
+instance LambdaSerializable TL.Text where
+  serialize = TL.toStrict
+  contentType _ = "text/plain; charset=utf8"
+
+instance LambdaSerializable String where
+  serialize = T.pack
+  contentType _ = "text/plain; charset=utf8"
+
+newtype ApplicationJson a = ApplicationJson a
+
+instance ToJSON a => LambdaSerializable (ApplicationJson a) where
+  serialize (ApplicationJson j) = TL.toStrict $ TLE.decodeUtf8 $ encode j
+  contentType _ = "application/json; charset=utf8"
+
+data Binary
+  = ImageGif_ ByteString
+  | ImageJpeg_ ByteString
+
+instance LambdaSerializable Binary where
+  serialize (ImageGif_ b)  = TE.decodeUtf8 $ B64.encode b
+  serialize (ImageJpeg_ b) = TE.decodeUtf8 $ B64.encode b
+  contentType (ImageGif_ _)  = "image/gif"
+  contentType (ImageJpeg_ _) = "image/jpeg"
+
+-- Smart constructors for export
+
+applicationJson :: a -> ApplicationJson a
+applicationJson = ApplicationJson
+
+imageGif :: ByteString -> Binary
+imageGif = ImageGif_
+
+imageJpeg :: ByteString -> Binary
+imageJpeg = ImageJpeg_
+
+instance LambdaSerializable a => ToJSON (ApiGatewayProxyResponse a) where
+  toJSON (ApiGatewayProxyResponse sc h b) =
+    object
+      [ "statusCode" .= sc
+      , "headers" .= insertWith (\_ old -> old) "Content-Type" (contentType b) h
+      , "body" .= serialize b
+      ]
