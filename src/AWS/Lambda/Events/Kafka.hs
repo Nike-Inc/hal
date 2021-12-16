aefa31aeb08354db9f142c440ec741ec00c91d08
Jack Kelly <jack.kelly@bellroy.com>
2021-12-16 16:24:33 +1000

    Support aeson ^>= 2.0

--- src/AWS/Lambda/Events/Kafka.hs
+++ src/AWS/Lambda/Events/Kafka.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP               #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs             #-}
@@ -48,7 +49,6 @@ import qualified Data.ByteString.Base64 as B64
 import           Data.List.NonEmpty     (NonEmpty)
 import qualified Data.List.NonEmpty     as NE
 import           Data.Function          ((&))
-import qualified Data.HashMap.Strict    as H
 import           Data.Int               (Int32, Int64)
 import           Data.Map               (Map)
 import           Data.Maybe             (catMaybes, maybeToList)
@@ -61,6 +61,26 @@ import           Data.Time.Clock.POSIX  (posixSecondsToUTCTime,
                                          utcTimeToPOSIXSeconds)
 import           Data.Vector            (Vector)
 import           GHC.Generics           (Generic)
+
+#if MIN_VERSION_aeson(2,0,0)
+import qualified Data.Aeson.Key         as Key
+import qualified Data.Aeson.KeyMap      as KM
+
+keyToText :: Key -> Text
+keyToText = Key.toText
+
+keyFromText :: Text -> Key
+keyFromText = Key.fromText
+#else
+import qualified Data.HashMap.Strict    as KM
+type Key = Text
+
+keyToText :: Key -> Text
+keyToText = id
+
+keyFromText :: Text -> Key
+keyFromText = id
+#endif
 
 -- | Represents an event from either Amazon MSK or a self-managed
 -- Apache Kafka cluster, as the payloads are very similar.
@@ -178,13 +198,13 @@ data Header = Header !Text !ByteString deriving (Eq, Show, Generic)
 
 instance FromJSON Header where
   parseJSON = withObject "header" $ \o ->
-    case H.toList o of
-      [(key, value)] -> Header key . B.pack <$> parseJSON value
+    case KM.toList o of
+      [(key, value)] -> Header (keyToText key) . B.pack <$> parseJSON value
       [] -> fail "Unexpected empty object"
       _ -> fail "Unexpected additional keys in object"
 
 instance ToJSON Header where
-  toJSON (Header key value) = object [key .= B.unpack value]
+  toJSON (Header key value) = object [keyFromText key .= B.unpack value]
 
 -- | Kafka timestamp types, derived from the Java client's enum at:
 -- https://github.com/apache/kafka/blob/trunk/clients/src/main/java/org/apache/kafka/common/record/TimestampType.java
